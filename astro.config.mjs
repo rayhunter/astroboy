b9b4f4335c054a38e476d57596f3e8db7a6ad78e
rayhunter <ray.hunter@gmail.com>
2023-06-18 21:45:26 -0700

    nsbrgr transition

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 import cloudflare from "@astrojs/cloudflare";
+import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: cloudflare()
+  adapter: cloudflare(),
+  integrations: [image()]
 });
